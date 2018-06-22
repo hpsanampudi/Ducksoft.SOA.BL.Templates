1d86fb90b7d97ab16a66223a3e11f6c5e4a7a9ce
hpsanampudi <hpsanampudi@gmail.com>
2018-06-22 18:45:03 +0100

    Updated nuget package related SOA common

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using System.Runtime.InteropServices;
 //      Build Number - MonthDate
 //      Revision - HoursMinutes
 //
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
 
